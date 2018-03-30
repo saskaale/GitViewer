ab0f20228c96a8ecd337e8a03aabe51804230eb9
ales_saska <mail@alessaska.cz>
2018-03-30 16:11:25 -0500

    little of code cleanup

--- gitviewer.js
+++ gitviewer.js
@@ -3,8 +3,8 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 
-var PORT = 88;
-var DEBUG = 0;
+var PORT = 8080;
+var DEBUG = 5;
 
 var mimes = {};
 
@@ -24,10 +24,12 @@ function load_mime_types(){
     var conf = fs.readFileSync('mimetypes.list').toString();
 
     conf.split(/\n/g).forEach(function(l){
-	var cols = l.split(/\s+/g);
-	if(cols.length < 2)
-	    return;
-	r[cols[0]] = cols[1];
+    	var cols = l.split(/\s+/g);
+    	if(cols.length < 2)
+  	    return;
+
+      let suffix = cols[0].substring(1);
+    	r[suffix] = cols[1];
     });
     return r;
 }
@@ -38,7 +40,6 @@ var staticfiles = loadStaticFiles();
 mimes = load_mime_types();
 
 console.log("Loaded MIME types");
-
 console.log("start running on localhost:"+PORT);
 
 function processQuery(q, req, res){
@@ -49,14 +50,16 @@ function processQuery(q, req, res){
 
 
     if(!/^\//g.test(q))
-	q = '/'+q;
+	     q = '/'+q;
 
     q = q.replace(/^\/([^\/]+)\/([^\/]+)\/blob/g, function(all, p1, p2){return "/"+p1+"/"+p2;});
 
     if(DEBUG)
-	console.log("query "+q);
+	    console.log("query "+q);
 
     var redirect = q;
+
+    console.log('redirecting to '+redirect);
 
 //    res.writeHead(200, {});
     res.writeHead(302, {'Location': redirect});
@@ -70,27 +73,28 @@ http.createServer(function (req, res) {
   if(url == '' || url == '/')
     url = '/index.html';
 
+  if(DEBUG)
+    console.log('GET '+url);
+
   var s = undefined;
   url.replace(/\/goto?(.+)$/g, function(all, get){s=get;});
 
   if(s){
     if(processQuery(s, req, res))
-	return;
+	     return;
   }
 
   var suffix = "";
-  suffix.replace(/\.([^\.]+)$/g, function(all, s){
+  url.replace(/\.([^\.]+)$/g, function(all, s){
     suffix = s;
   });
-  var mime = mimes[suffix];
+  var mime = mimes[suffix||'txt'];
 
   if(mime === ""){
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("Unknown type of file");
     return;
   }
-
-
 
   //serve static
   if(staticfiles[url]){
@@ -104,41 +108,34 @@ http.createServer(function (req, res) {
 
   var u = 'https://raw.githubusercontent.com'+url;
   if(DEBUG)
-    console.log(u);
+    console.log('... getted from '+u);
 
-https.get(u, function(clientres){
-  clientres.on('data', function(d){
-        if(DEBUG)
-	    console.log('data');
-	if(!data_received){
-	    res.writeHead(200, {'Content-Type': mime});
-	    data_received = true;
-	}
-        res.write(d);
-  });
-      clientres.on('error', function(e){
+  https.get(u, function(clientres){
+    clientres.on('data', function(d){
+  	  if(!data_received){
+  	     res.writeHead(200, {'Content-Type': mime});
+  	     data_received = true;
+      }
+      res.write(d);
+    });
+    clientres.on('error', function(e){
         if(DEBUG){
-	    console.log('error');
-	    console.error(e);
-	}
-	res.writeHead(502);
+    	     console.log('error');
+    	     console.error(e);
+    	  }
+    	  res.writeHead(502);
         res.end("502 - Internal server error");
-      });
-      clientres.on('end', function(){
-        if(DEBUG)
-	    console.log('end');
-	res.end();
-     })
-
-}).on('error', function(e){
-        if(DEBUG){
-	    console.log('error main');
-	    console.error(e);
-	}
-	res.writeHead(502);
-        res.end("502 - Internal server error (file not found)");
-});
+    });
+    clientres.on('end', function(){
+       res.end();
+    })
+  }).on('error', function(e){
+    if(DEBUG){
+  	    console.log('error main');
+  	    console.error(e);
+  	}
+  	res.writeHead(502);
+    res.end("502 - Internal server error (file not found)");
+  });
 
 }).listen(PORT);
-
-
